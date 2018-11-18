63cf577b3e73f2c3f945d9bec29e6a9de09a4320
Olya Tribushinina <olga.tribushinina@mail.ru>
2018-11-18 20:16:07 +0300

    add js file, removed extra dimensions in rem

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+const $hamburger = $(".js-open-menu");
+const $fixedMenu = $(".js-fixed-menu");
+const $closeMenu = $(".js-close-menu"); 
+
+
+$hamburger.on("click", function() {
+	$fixedMenu.fadeIn().addClass("fixed-menu--open");
+	$("body").addClass("blocked-scroll");
+});
+
+// $(.hamburger-menu-btn).on("click", function(e) {
+// 	$(this).addClass("hamburger-menu-btn--active");
+// });
+
+$closeMenu.on("click", function () {
+	$fixedMenu.fadeOut().removeClass("fixed-menu--open");
+	$("body").removeClass("blocked-scroll");
+});
+
