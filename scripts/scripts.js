9d3f1fdbdfd6108b5688f9d8b420cf758bf88bf2
Olya Tribushinina <olga.tribushinina@mail.ru>
2018-11-19 14:44:30 +0300

    made linear-gradient for slider, remade js file + classes of elements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,15 @@
 const $hamburger = $(".js-open-menu");
-const $fixedMenu = $(".js-fixed-menu");
-const $closeMenu = $(".js-close-menu"); 
 
 
-$hamburger.on("click", function() {
-	$fixedMenu.fadeIn().addClass("fixed-menu--open");
-	$("body").addClass("blocked-scroll");
+$hamburger.on("click", function(e) {
+	$(this).toggleClass(".js-open-menu--active");
+	if ($hamburger.hasClass(".js-open-menu--active")) {
+		$hamburger.fadeIn().addClass("fixed-menu--open");
+		$("body").addClass("blocked-scroll");
+	}else {
+		$hamburger.fadeOut().removeClass.addClass("fixed-menu--open"); 
+		$("body").removeClass("blocked-scroll");
+	}
 });
 
-$(".hamburger-menu-btn").on("click", function(e) {
-	$(this).addClass("hamburger-menu-btn--active");
-});
-
-$closeMenu.on("click", function () {
-	$fixedMenu.fadeOut().removeClass("fixed-menu--open");
-	$("body").removeClass("blocked-scroll");
-});
 
