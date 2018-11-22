25ea051368bca7109bc1399bba8cf4113c4690d2
Olya Tribushinina <olga.tribushinina@mail.ru>
2018-11-22 14:34:22 +0300

    made functional for accordeon-element

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,3 +13,28 @@ $hamburger.on("click", function() {
 });
 
 
+
+function accordeon(btn) {
+	$(btn).on("click", function () {
+		let thisBtn = this;
+
+		$(btn).each(function (index, element) {
+			let accordItem = $(this).parent();
+
+			if (thisBtn == element) {
+				if (accordItem.hasClass("active-js")) {
+					accordItem.removeClass("active-js");
+				} else {
+					accordItem.addClass("active-js");
+				}
+			} else {
+				if (accordItem.hasClass("active-js")) {
+					accordItem.removeClass("active-js");
+				}
+			}
+		})
+	});
+}
+
+accordeon(".accordeon__button");
+accordeon(".accordeon__button_vertical");
